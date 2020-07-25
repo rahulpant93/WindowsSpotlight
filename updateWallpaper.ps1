8428e15f8d3dec43596208bce4c4e0ae4cdcd420
rahulpant93 <rahipant@gmail.com>
2020-07-25 09:37:55 +0530

    Initial commit

---
+++ updateWallpaper.ps1
@@ -0,0 +1,32 @@
+﻿$sid = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).User.Value
+$path = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative\' + $sid
+$items = Get-ChildItem -Path $path | Select-Object Name
+
+$largestKey = [bigint]$items.Name.Split("\")[-1]
+
+$items | ForEach-Object {
+    $item = [bigint]$_.Name.Split("\")[-1]
+   if($item -gt $largestKey){
+    $largestKey = $item 
+    }
+}
+$path = $path +"\"+$largestKey
+$landscapeImage = (Get-ItemProperty -Path $path -Name "landscapeImage").landscapeImage
+
+$setwallpapersrc = @"
+using System.Runtime.InteropServices;
+public class wallpaper
+{
+public const int SetDesktopWallpaper = 20;
+public const int UpdateIniFile = 0x01;
+public const int SendWinIniChange = 0x02;
+[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+public static void SetWallpaper ( string path )
+{
+SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+}
+}
+"@
+Add-Type -TypeDefinition $setwallpapersrc
+[wallpaper]::SetWallpaper($landscapeImage)
---
+++ updateWallpaper.vbs
@@ -0,0 +1,3 @@
+command = "powershell.exe -nologo -command E:\updateWallpaper.ps1"
+ set shell = CreateObject("WScript.Shell")
+ shell.Run command,0
